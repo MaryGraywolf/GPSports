9af4564233124d9e1e425cb256f447ffc6030a66
Glauber de Sousa Fernandes <glauber.fernandes@tbmtextil.com.br>
2023-05-17 23:04:57 -0300

    testando o commit como colaborador

--- gpsportsmobile/src/components/Loading.js
+++ gpsportsmobile/src/components/Loading.js
@@ -1,6 +1,7 @@
 import * as React from "react";
 import {Spinner, Center, NativeBaseProvider} from 'native-base';
 
+//testando o commit como colaborador
 export function Loading() {
     return (
         <Center flex={1} bg="gray.900">
